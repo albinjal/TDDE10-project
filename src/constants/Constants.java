aa3e8331b243109bd3e0438f6da75feea141fabc
albja664 <albja664@roxen2-117.ad.liu.se>
2020-02-28 17:11:20 +0100

    initial coding

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -1,5 +1,10 @@
 package constants;
 
-public interface Constants {
+import states.GameState;
+import states.PlayState;
 
+public interface Constants {
+	GameStates entryState = GameStates.Play;
+    int SCREEN_WIDTH = 1000;
+    int SCREEN_HEIGHT = 800;
 }
--- src/states/GameModel.java
+++ src/states/GameModel.java
@@ -1,5 +1,21 @@
 package states;
 
-public class GameModel {
+import constants.GameStates;
 
+public class GameModel {
+	private GameState activeState;
+	public GameModel() {
+		
+	}
+	
+	public void swtichState(GameState state) {
+		this.activeState = state;
+	}
+	
+	public GameState newState(GameStates state) {
+		switch(state) {
+			case Play: return new PlayState(this); 
+		}
+		return null;
+	}
 }
